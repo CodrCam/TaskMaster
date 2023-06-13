17c680b8207d72c405c3b5573ce7ec8800aa5928
Cameron Griffin (He/Him) <ct.griffin7@gmail.com>
2023-06-13 16:04:45 -0700

    added teams feature

--- app/src/main/java/com/taskApp/taskmaster/MainActivity.java
+++ app/src/main/java/com/taskApp/taskmaster/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,8 +17,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.Task;
+import com.amplifyframework.datastore.generated.model.Team;
 import com.taskApp.camtaskmaster.R;
-
 
 import java.util.ArrayList;
 
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnTas
     ArrayList<Task> tasks = new ArrayList<>();  // List to hold tasks
     private ActivityResultLauncher<Intent> activityResultLauncher;
     private TaskAdapter taskAdapter;
+    private Team selectedTeam; // Add this line
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +49,7 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnTas
                             Task task = Task.builder()
                                     .title(newTask)
                                     .details(newTaskDetail)
+                                    .team(selectedTeam)  // Associate task with selected team
                                     .build();
 
                             Amplify.DataStore.save(task,
@@ -62,8 +65,6 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnTas
                                         error.printStackTrace();
                                     }
                             );
-
-
                         }
                     }
                 }
@@ -96,10 +97,36 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnTas
 
         TextView textViewTitle = findViewById(R.id.textViewTitle);
         textViewTitle.setText(getString(R.string.task_title, username));
+
+
+        // fetch the team from shared preferences (assuming you have saved it there)
+        String teamId = sharedPref.getString("teamId", "");
+
+        Amplify.DataStore.query(
+                Team.class,
+                Team.ID.eq(teamId),
+                team -> {
+                    if (team.hasNext()) {
+                        Team fetchedTeam = team.next();
+                        selectedTeam = fetchedTeam;
+                        runOnUiThread(() -> {
+                            tasks.clear();
+                            if(selectedTeam.getTasks() != null) {
+                                for (Task task : selectedTeam.getTasks()) {
+                                    tasks.add(task);
+                                }
+                            }
+                            taskAdapter.notifyDataSetChanged();
+                        });
+                    }
+                },
+                failure -> Log.e("Team query", "Could not query DataStore", failure)
+        );
     }
 
+
     @Override
-    public void onTaskClick(com.amplifyframework.datastore.generated.model.Task task) {
+    public void onTaskClick(Task task) {
         Intent intent = new Intent(this, TaskDetailActivity.class);
         intent.putExtra("taskTitle", task.getTitle());
         intent.putExtra("taskDetail", task.getDetails());
@@ -108,12 +135,12 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnTas
     }
 
     @Override
-    public void onDoingClick(com.amplifyframework.datastore.generated.model.Task task) {
+    public void onDoingClick(Task task) {
         taskAdapter.notifyDataSetChanged();
     }
 
     @Override
-    public void onDoneClick(com.amplifyframework.datastore.generated.model.Task task) {
+    public void onDoneClick(Task task) {
         taskAdapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/taskApp/taskmaster/SettingsActivity.java
+++ app/src/main/java/com/taskApp/taskmaster/SettingsActivity.java
@@ -1,5 +1,13 @@
 package com.taskApp.taskmaster;
 
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Team;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
@@ -11,7 +19,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.taskApp.camtaskmaster.R;
 
 
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+
 public class SettingsActivity extends AppCompatActivity {
+    private Spinner teamSpinner;
+    private List<Team> teams = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,32 @@ public class SettingsActivity extends AppCompatActivity {
         String username = sharedPref.getString("username", "");
         editTextUsername.setText(username);
 
+        teamSpinner = findViewById(R.id.teamSpinner);
+
+        // Define the team names
+        String[] teamNames = {"Seahawks", "4whiners", "Lambs", "Baseball"};
+
+        // Create teams and add them to the list
+        for (String teamName : teamNames) {
+            Team team = Team.builder()
+                    .name(teamName)
+                    .build();
+
+            teams.add(team);
+        }
+
+        // Set teams to spinner
+        ArrayAdapter<String> teamsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, teamNames);
+        teamSpinner.setAdapter(teamsAdapter);
+
+        // Save each team
+        for (Team team : teams) {
+            Amplify.DataStore.save(team,
+                    success -> Log.i("Tutorial", "Saved item: " + success.item().getName()),
+                    error -> Log.e("Tutorial", "Could not save item to DataStore", error)
+            );
+        }
+
         Button buttonSave = findViewById(R.id.buttonSave);
         buttonSave.setOnClickListener(v -> {
             String newUsername = editTextUsername.getText().toString();
@@ -32,10 +71,19 @@ public class SettingsActivity extends AppCompatActivity {
                 return;
             }
 
+            String selectedTeam = teamSpinner.getSelectedItem().toString();
+            if (selectedTeam.isEmpty()) {
+                Toast.makeText(SettingsActivity.this, "Please select a team", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
             SharedPreferences.Editor editor = sharedPref.edit();
             editor.putString("username", newUsername);
+            editor.putString("team", selectedTeam);
             editor.apply();
             finish();
         });
     }
+
+
 }
